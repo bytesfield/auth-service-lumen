608f717f9422611b28f118bf24eebea93276e759
Abraham Udele <abrahamudele@gmail.com>
2020-09-23 18:38:20 +0100

    Fixed Api and Routes

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,8 @@
 
 use App\User;
 use Faker\Generator as Faker;
+use Carbon\Carbon;
+use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,7 +20,12 @@ use Faker\Generator as Faker;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->email,
+        'firstname' => $faker->word,
+        'lastname' => $faker->word,
+        'phone' => $faker->e164PhoneNumber,
+        'email' => $faker->unique()->safeEmail,
+        'email_verified_at' => Carbon::now(),
+        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'remember_token' => Str::random(10),
     ];
 });
---
+++ tests/UserAuthTest.php
@@ -0,0 +1,157 @@
+<?php
+
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+use Illuminate\Support\Str;
+use App\User;
+use App\Http\Controllers\Api\AuthenticationController;
+use Tymon\JWTAuth\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+
+class UserAuthTest extends TestCase
+{
+
+
+     /**
+     * A basic unit test example.
+     *
+     * @test
+     * @return void
+     */
+
+    public function user_can_register()
+    {
+
+        $password = $this->faker->password;
+        $data = [
+            'firstname' => $this->faker->name,
+            'lastname' => $this->faker->name,
+            'phone' => $this->faker->e164PhoneNumber,
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => $password, // password
+            'password_confirmation' => $password,
+        ];
+
+        $response = $this->json('POST', '/api/register', $data);
+
+        $response->seeStatusCode(200);
+
+    }
+
+    /** @test */
+    public function user_can_login_with_correct_credentials()
+    {
+
+        $password = '123456';
+
+        $user = factory(User::class)->create([
+
+            'password' => Hash::make($password),
+        ]);
+
+        $credentials = [
+
+            'email' => $user->email,
+            'password' => $password,
+        ];
+
+        $response = $this->json('POST', '/api/login', $credentials);
+
+
+        $response->seeStatusCode(200);
+        $this->actingAs($user, 'api');
+
+    }
+
+
+     /** @test */
+    public function user_cannot_login_with_incorrect_password()
+    {
+        $password = $this->faker->password;
+        $invalid_password = $this->faker->password;
+
+        $user = factory(User::class)->create([
+
+            'password' => $password,
+        ]);
+
+        $credentials = [
+
+            'email' => $user->email,
+            'password' => $invalid_password,
+        ];
+
+        $response = $this->json('POST', '/api/login', $credentials);
+
+        $response->seeStatusCode(422);
+    }
+
+     /** @test */
+     public function user_cannot_login_with_incorrect_email()
+     {
+
+        $email = $this->faker->unique()->safeEmail;
+        $wrong_email = $this->faker->unique()->safeEmail;
+        $password = $this->faker->password;
+
+         $user = factory(User::class)->create([
+
+             'email' => $email,
+         ]);
+
+         $credentials = [
+
+             'email' => $wrong_email,
+             'password' => $password,
+
+         ];
+
+         $response = $this->json('POST', '/api/login', $credentials);
+
+         $response->seeStatusCode(422);
+
+     }
+
+     /** @test */
+    public function get_all_users_if_route_exist() {
+
+        $response = $this->json('GET','/api/getUsers');
+
+        $response->seeStatusCode(200);
+
+    }
+
+    /** @test */
+    public function all_users_can_be_viewed() {
+
+
+        $users = factory(User::class, 2)->create()->map(function ($users) {
+            return $users->only(['id','firstname', 'lastname','phone','email','status']);
+        });
+
+        $response = $this->json('GET','/api/getUsers');
+
+        $response->seeStatusCode(200);
+
+    }
+
+    /** @test */
+    public function user_can_generate_new_token() {
+
+        $user = factory(User::class)->create();
+
+        $token = auth('api')->tokenById($user->id);
+
+        //$response = $this->withHeaders(['Authorization' => 'Bearer '.$token])
+        //->json('GET', '/api/generateToken');
+
+        $response = $this->json('GET', '/api/generateToken',  ['HTTP_Authorization' => 'Bearer '.$token]);
+        //$this->assertEquals(422, $this->response->status());
+
+
+        $response->seeStatusCode(200);
+
+    }
+}
